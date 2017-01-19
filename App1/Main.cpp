6ba087c2162a7a44ce9eec980b47f48642f92fb1
deviantdes <des-@live.com.sg>
2017-01-19 16:24:02 +0800

    initia

---
+++ App1/Main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "header.h"
+
+int main()
+{
+	int choice =0;
+
+	printf("total is %i and %.3f",add(1,2),janArray[0][0]);
+	scanf("%i",&choice);
+}
---
+++ App1/header.h
@@ -0,0 +1,7 @@
+#ifndef _header_h
+#define _header_h
+
+extern int add(int x, int y);
+extern double janArray[3][31];
+
+#endif
